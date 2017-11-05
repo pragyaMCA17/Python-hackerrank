9d9dcb99800e0b15edce859e2c863a76f97f8511
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-11-06 00:02:56 +0530

    Function to learn if else condition

---
+++ if else.py
@@ -0,0 +1,10 @@
+def function():
+    number = int(input())
+    if (n % 2 != 0 ):
+        print("Weird")
+    elif (n >=6 and n <= 20):
+        print("Weird")
+    else:
+        print("Not Weird")
+
+    
