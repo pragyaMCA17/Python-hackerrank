a3954588e84e8c4c8f57cf734b9105fa2f64e0d1
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2017-12-26 00:49:25 +0530

    Function to perform arthematic operation

---
+++ arthematic operation.py
@@ -0,0 +1,6 @@
+def arthematic():
+    first_no = float(input())
+    second_no = float(input())
+    print("sum of two number is =",first_no+second_no)
+    print("subtraction of two number is =",first_no-second_no)
+    print("multiply of two number is =",first_no*second_no)
