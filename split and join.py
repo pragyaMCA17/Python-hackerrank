7b5bd2602d901002487a73246890c1baa386529b
PRAGYA RASTOGI <31646309+pragyaMCA17@users.noreply.github.com>
2018-10-14 12:05:49 -0700

    added main function and made user driven(input)

--- split and join.py
+++ split and join.py
@@ -2,3 +2,10 @@ def split_and_join(line):
     line=line.split(" ")
     line=  "-".join(line)
     return line
+
+if__name__=="__main__" :
+   # line ="This is an example of split and join"
+    line= input("Enter the line : ")
+    print("Line , before function call: ",line)
+    print("Line , after function call :",split_and_join(line))
+    
