5bbae918a338b99c783dd9035c3af7a02bdad4df
PRAGYA RASTOGI <31646309+pragyaMCA17@users.noreply.github.com>
2018-10-14 11:49:49 -0700

    input function modified and comments added

--- for loop.py
+++ for loop.py
@@ -1,4 +1,9 @@
 def forLoop():
-    number = int(input())
+    '''
+    Functionality of for loop 
+    '''
+    #number : 5(say)
+    number = int(input("Enter number of iterations"))
+    #output : 0 , 1 , 4 , 9 , 16
     for  i in range (number):
         print(i*i)
